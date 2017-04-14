bdda8ee8812f845b9718aa05b938c541a6bd7e66
yaoyizhi <email@yaoadou888>
2017-04-14 16:54:58 +0800

    add publish&hide button

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,4 +4,14 @@ class Job < ApplicationRecord
     validates :wage_upper_bound, presence: true
     validates :wage_lower_bound, numericality: { greater_than: 0 }
     scope :recent, -> { order('created_at DESC') }
+
+    def publish!
+        self.is_hidden = false
+        save
+    end
+
+    def hide!
+        self.is_hidden = true
+        save
+    end
 end
